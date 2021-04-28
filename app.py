0609360f27bcfe59a2110f56cd11fe0c5f4f6939
Bence <bence@Bences-MacBook-Air.local>
2021-04-28 16:45:10 +0200

    heroku app created

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st 
 import pandas as pd
+import numpy as np
+from PIL import Image
+from plotly.subplots import make_subplots
 import plotly.express as px 
 import plotly.graph_objects as go
 import time
@@ -14,13 +17,13 @@ def load_data():
     data = pd.read_csv('COVID_data')
     data['week'] = data.year_week.apply(lambda x: convert(x))
     return data
+data = load_data()
 
 st.title("COVID-19 data")
 
-data = load_data()
-country = st.sidebar.selectbox("Select a country", data['country'])
-selected_country = data[data.country == {country}]
-st.write(f"The selected country is {country}")
+country_select = st.sidebar.selectbox("Select a country", data['country'].unique())
+selected_country = data[data['country'] == country_select]
+st.write(f"The selected country is {selected_country}")
 
 fig = px.line(data_frame = selected_country, x = 'week', y = 'cumulative_count', color = 'indicator')
 
