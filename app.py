d259e6cd7bd812460511839e52fa0dff1ecfa20d
Bence <bence@bences-air.t.hu>
2021-04-28 02:29:49 +0200

    update

--- app.py
+++ app.py
@@ -4,27 +4,31 @@ import plotly.express as px
 import plotly.graph_objects as go
 import time
 
-@st.cache
+def convert(x):
+    year, week = x.split('-')
+    year = (int(year) - 2020) * 54
+    return year + int(week)
+
+
 def load_data():
-    data = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv')
+    data = pd.read_csv('COVID_data')
     data['week'] = data.year_week.apply(lambda x: convert(x))
     return data
 
 st.title("COVID-19 data")
 
-country = st.sidebar.selectbox("Select a country", "{country}")
+data = load_data()
+country = st.sidebar.selectbox("Select a country", data['country'])
+selected_country = data[data.country == {country}]
 st.write(f"The selected country is {country}")
 
-def convert(x):
-    year, week = x.split('-')
-    year = (int(year) - 2020) * 54
-    return year + int(week)
-    
-data = load_data()
-
-selected_country = data[data.country == {country}]
-
-fig = px.line(data_frame = selected_country, x = 'week', y = 'cumulative_count', color = 'indicator', )
+fig = px.line(data_frame = selected_country, x = 'week', y = 'cumulative_count', color = 'indicator')
 
 st.plotly_chart(fig)
+
+
+
+
+
+
 
